b0311f48ea5f314ca477418c344d4a7c5ab7ab91
Thiago Sequeira <73362049+thsequeira@users.noreply.github.com>
2020-12-23 16:40:58 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,87 @@
+const typed = new Typed(".typed",   {
+    strings:   
+     [
+    "<i>Full Stack Developer</i>",
+     "<i>Full Stack Developer</i>"
+    ],
+    //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	//smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active');
+});
+
+const navbar = document.querySelector(".navbar");
+const homebg = document.querySelector(".home__background");
+const menu = document.querySelector(".navbar__menu");
+const links = document.querySelector(".navbar__links");
+const sections = document.querySelectorAll("section");
+const config = {
+  rootMargin: "0px",
+  threshold: [0.6, 0.9],
+};
+
+function handleLlinks() {
+  if (window.innerWidth <= 991) {
+    links.classList.toggle("visible");
+  }
+}
+
+menu.addEventListener("click", handleLlinks);
+links.addEventListener("click", handleLlinks);
+
+
+window.addEventListener("scroll", function () {
+  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
+  window.scrollY < 100 && (navbar.style.background = `transparent`);
+});
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      intersectionHandler(entry);
+    }
+  });
+}, config);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+function intersectionHandler(entry) {
+  const id = entry.target.id;
+  const currentlyActive = document.querySelector(".navbar-links  .active");
+  const shouldBeActive = document.querySelector(
+    ".navbar-links [data-ref=" + id + "]"
+  );
+
+  if (currentlyActive) {
+    currentlyActive.classList.remove("active");
+  }
+  if (shouldBeActive) {
+    shouldBeActive.classList.add("active");
+  }
+}
+
+window.onmousemove = (event) => {
+    document.querySelectorAll('.move').forEach(element => {
+        const data = element.getAttribute('data-value')
+        const x = (window.innerWidth - event.pageX * data) / 100
+        const y = (window.innerHeight - event.pageY * data) / 100
+        element.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
