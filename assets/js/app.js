2a3f37c1600db1e44b420a420ed6464aebcff1a8
unknown <thiagosequeira02@gmail.com>
2021-07-13 18:24:49 -0300

    subiendo cambios

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+const seccionesPagina = new fullpage('#fullpage', {
+	// ──────────────────────────────────────────────────
+	//   :::::: Opciones Basicas
+	// ──────────────────────────────────────────────────
+	autoScrolling: true, // Se activa el scroll.
+	fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+	fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+	easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
+	scrollingSpeed: 800, // Velocidad del scroll. Valores: en milisegundos.
+	css3: true, // Si usara CSS3 o javascript.
+	easingcss3: 'ease-out', // Curva de velocidad del efecto.
+	loopBottom: false,
+	// ──────────────────────────────────────────────────
+	//   :::::: Barra de navegación
+	// ──────────────────────────────────────────────────
+	navigation: true, // Muesta la barra de navegación.
+	menu: '#menu', // Menu de navegación.
+	anchors: ['Inicio', 'Proyectos', 'Contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+	navigationTooltips: ['', '', ''], // Tooltips que mostrara por cada boton.
+	showActiveTooltip: false, // Mostrar tooltip activa.
+	// ──────────────────────────────────────────────────
+	//   :::::: Secciones
+	// ──────────────────────────────────────────────────
+	verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+	// ──────────────────────────────────────────────────
+	//   :::::: Slides
+	// ──────────────────────────────────────────────────
+	controlArrows: false, // Flechas del slide
+	slidesNavigation: false, // Indicadores del slide
+	afterLoad: function(origin, destination){
+		if(destination.anchor == 'contacto'){
+			document.querySelector('.footer h2').style.opacity = 1;
+		}
+	}
+});
